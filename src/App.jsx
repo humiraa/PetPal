2a4f2410311826f35bd577e1c782c069593e1ffc
Gavin Yip <gavin.yip88@gmail.com>
2023-11-10 14:00:01 +0000

    function bars

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,56 @@
-import { useState } from 'react';
-import './App.css';
-import Header from './Header';
-import Footer from './Footer';
-import PetImage from './Pet';
-import AdoptComponent from './Adopt';
-import Namepet from './NamePet';
-import Playarea from './Playarea';
+import { useState } from "react";
+import "./App.css";
+import Header from "./Header";
+import Footer from "./Footer";
+import PetImage from "./Pet";
+import AdoptComponent from "./Adopt";
+import Namepet from "./NamePet";
+import Playarea from "./Playarea";
 
 function App() {
   // states:
   // a pet name for user to input
-  const [petName, setPetName] = useState('');
+  const [petName, setPetName] = useState("");
   // a fullness state to indicate the pet's hunger level
   const [fullness, setFullness] = useState(50);
   // a happiness state to indicate the pet's happiness level
   const [happiness, setHappiness] = useState(50);
   // a state to switch between different components
-  const [currentPage, setCurrentPage] = useState('adopt-page');
+  const [currentPage, setCurrentPage] = useState("adopt-page");
 
   /// helper functions
 
   // handle adopt and set page to name pet
 
   function handleAdopt() {
-    setCurrentPage('nameApet');
+    setCurrentPage("nameApet");
   }
   // name pet and set page to play-area
 
   function handleNamePet(name) {
     setPetName(name);
-    setCurrentPage('play-area');
+    setCurrentPage("play-area");
   }
   // function to update fullness of a pet
   function handleFeed() {
     // logic to update pet's fullness level
+    setFullness((prevFullness) => Math.min(prevFullness + 10, 100));
   }
   function handlePlay() {
     // logic to update pet's happiness level
+    setHappiness((prevHappiness) => Math.min(prevHappiness + 10, 100));
   }
   function handleBath() {
     // logic to update pet's happiness level
+    setHappiness((prevHappiness) => Math.min(prevHappiness + 5, 100));
   }
 
   return (
     <div className="App">
       <Header />
-      {currentPage === 'adopt-page' && <AdoptComponent onAdopt={handleAdopt} />}
-      {currentPage === 'nameApet' && <Namepet onNamePet={handleNamePet} />}
-      {currentPage === 'play-area' && (
+      {currentPage === "adopt-page" && <AdoptComponent onAdopt={handleAdopt} />}
+      {currentPage === "nameApet" && <Namepet onNamePet={handleNamePet} />}
+      {currentPage === "play-area" && (
         <Playarea
           petName={petName}
           petFullness={fullness}
--- src/Playarea.jsx
+++ src/Playarea.jsx
@@ -4,12 +4,18 @@ function Playarea(props) {
       <img src="src/puppy.png" />
       <h3>{props.petName} </h3>
       <p>Happiness</p>
-      <progress id="health" value="50" max="100"></progress>
+      <progress id="health" value={props.petHappiness} max="100"></progress>
       <p>Hunger</p>
-      <progress id="hunger" value="50" max="100"></progress>
-      <button className="play">play</button>
-      <button className="feed">Feed</button>
-      <button className="bath">Bath</button>
+      <progress id="hunger" value={props.petFullness} max="100"></progress>
+      <button className="play" onClick={props.onPlay}>
+        play
+      </button>
+      <button className="feed" onClick={props.onFeed}>
+        Feed
+      </button>
+      <button className="bath" onClick={props.onBath}>
+        Bath
+      </button>
     </>
   );
 }
