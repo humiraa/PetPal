dbf4c9437fc2dc16e0a677f1a17333dd0ea9307f
Humairaa Patel <humairaapatel@Humairaas-MacBook-Air.local>
2023-12-09 16:44:21 +0000

    pp

--- src/Pet.jsx
+++ src/Pet.jsx
@@ -2,6 +2,6 @@
 
 export default function PetImage(){
     return(<>
-    <img src="./public/Pet-img.svg" alt="dog" className="petImage"></img>
+    <img src="/Pet-img.svg" alt="dog" className="petImage"></img>
     </>)
 }
