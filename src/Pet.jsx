6f696e65876e63f273240c27d194b28e8873d9d2
Humairaa Patel <humairaapatel@Humairaas-MacBook-Air.local>
2023-12-09 16:40:16 +0000

    images fix

--- src/Pet.jsx
+++ src/Pet.jsx
@@ -2,6 +2,6 @@
 
 export default function PetImage(){
     return(<>
-    <img src="./public/Pet-img.png" alt="dog" className="petImage"></img>
+    <img src="./public/Pet-img.svg" alt="dog" className="petImage"></img>
     </>)
 }
